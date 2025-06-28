fc41dfe3dd08730386391a1f3c5b000ad616bda0
kishorekumarj79 <kishore.j@vlinder.io>
2025-06-28 10:57:05 +0530

    vercel commit

--- admin-side/admin-alumni-form/src/api/formDataApi.jsx
+++ admin-side/admin-alumni-form/src/api/formDataApi.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/users';
+const BASE_URL = 'https://alumni-job-form.onrender.com/api/users';
 
 export const getAllUsers = () => axios.get(BASE_URL);
 
